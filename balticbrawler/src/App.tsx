75e5d8239fc339c5e8e67ef2210293bd4cb18a4e
Mirco Kähler <m_irco@live.de>
2024-07-15 22:25:42 +0200

    Use dark theme for page

--- balticbrawler/src/App.tsx
+++ balticbrawler/src/App.tsx
@@ -1,14 +1,28 @@
-import { useState } from "react";
 import viteLogo from "/BB_logo.jpeg";
 import "./App.css";
-import { Container } from "@mui/material";
+import Container from "@mui/material/Container";
+import { CssBaseline, ThemeProvider, colors, createTheme } from "@mui/material";
 
 function App() {
+    const themeDark = createTheme({
+        palette: {
+            background: {
+                default: colors.blueGrey[900],
+            },
+            text: {
+                primary: "#ffffff",
+            },
+        },
+    });
+
     return (
-        <a>
-            <h2>Hier entsteht die Seite der Baltic Brawler</h2>
-            <img src={viteLogo} height={300} />
-        </a>
+        <ThemeProvider theme={themeDark}>
+            <CssBaseline />
+            <Container>
+                <h2>Hier entsteht die Seite der Baltic Brawler</h2>
+                <img src={viteLogo} height={300} />
+            </Container>
+        </ThemeProvider>
     );
 }
 
