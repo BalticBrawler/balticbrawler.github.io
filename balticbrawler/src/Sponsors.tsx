0f523e4bf4ad723dfd4997f1c6c3a6aa41196a7a
Mirco Kähler <m_irco@live.de>
2024-10-23 18:59:13 +0200

    Add TableTopBasics sponsor

--- balticbrawler/src/Sponsors.tsx
+++ balticbrawler/src/Sponsors.tsx
@@ -1,17 +1,20 @@
 import { Box, Card, Link, Typography } from "@mui/material";
 import UrbanArtworkIcon from "/urban_artworx_logo.avif";
 import WarfactorIcon from "/warfactor_logo.png";
+import TableTopBasics from "/TableTopBasics.jfif";
 
 function Sponsors() {
     const sponsors = [
         {
+            img: TableTopBasics,
+            link: "https://tabletopbasics.de/",
+        },
+        {
             img: UrbanArtworkIcon,
-            title: "test sp",
             link: "https://urban-artworx.com/",
         },
         {
             img: WarfactorIcon,
-            title: "test sp",
             link: "https://warfactor.eu/",
         },
     ];
@@ -32,11 +35,12 @@ function Sponsors() {
                 flex={1}
                 alignContent="center"
                 alignSelf="center"
+                alignItems="center"
                 display="flex"
                 flexWrap="wrap"
             >
                 {sponsors.map((x) => (
-                    <Link m={1} href={x.link}>
+                    <Link m={1} href={x.link} target="_blank">
                         <img src={x.img} width={300}></img>
                     </Link>
                 ))}
